5803eeed0a4aee5505bd2ce7b32384b6d6080bd6
Daniel-Dev23 <danieldev.info@gmail.com>
2022-09-20 23:48:11 -0700

    feat: The "About" section has been completed.
    - The "Who am I?" section has been implemented.
    - The "Preparation" section has been implemented.
    - The "Contact" section has been implemented.

---
+++ src/js/custom/PreparationCustom.js
@@ -0,0 +1,9 @@
+import PreparationHTML from '../../pages/custom/Preparation.html';
+
+export const PreparationCustom = () => {
+
+    const $PreparationCustom = PreparationHTML;
+    
+    return $PreparationCustom;
+
+}
---
+++ src/js/custom/WhoCustom.js
@@ -0,0 +1,9 @@
+import WhoHTML from '../../pages/custom/Who.html';
+
+export const WhoCustom = () => {
+
+    const $WhoCustom = WhoHTML;
+    
+    return $WhoCustom;
+
+}
